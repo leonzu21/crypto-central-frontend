9c72bfd631857d68d586efbd712c597d15f37dde
Iulian Leon <iulian.leon@philro.ro>
2022-02-02 18:25:05 +0200

    remove fetch request here

--- src/pages/whale-alerts.js
+++ src/pages/whale-alerts.js
@@ -19,25 +19,25 @@ import { WhaleAlertListResults } from "../components/whale-alert/whale-alert-lis
 import { WhaleAlertListToolbar } from "../components/whale-alert/whale-alert-list-toolbar";
 
 
-const fetcher = (url) => fetch(url).then((res) => res.json());
+// const fetcher = (url) => fetch(url).then((res) => res.json());
 
 
 const WhaleAlerts = () => {
-  const { data, error, mutate, size, setSize, isValidating } = useSWRInfinite(
-    (index) =>
-      `http://localhost:5000/api/transactions/search/findAllByDayOrderByAmountDesc?theDate=2022-02-01&page=${index}`,
-    fetcher
-  );
-  if (!data) return "Loading...";
+  // const { data, error, mutate, size, setSize, isValidating } = useSWRInfinite(
+  //   (index) =>
+  //     `http://localhost:5000/api/transactions/search/findAllByDayOrderByAmountDesc?theDate=2022-02-01&page=${index}`,
+  //   fetcher
+  // );
+  // if (!data) return "Loading...";
 
-  const transactions = data ? [].concat(...data) : [];
-  const isLoadingInitialData = !data && !error;
-  const isLoadingMore =
-    isLoadingInitialData ||
-    (size > 0 && data && typeof data[size - 1] === "undefined");
-  const isEmpty = data?.[0]?.length === 0;
-  const isReachingEnd = isEmpty || (data && data[data.length - 1]?.length < 6);
-  const isRefreshing = isValidating && data && data.length === size;
+  // const transactions = data ? [].concat(...data) : [];
+  // const isLoadingInitialData = !data && !error;
+  // const isLoadingMore =
+  //   isLoadingInitialData ||
+  //   (size > 0 && data && typeof data[size - 1] === "undefined");
+  // const isEmpty = data?.[0]?.length === 0;
+  // const isReachingEnd = isEmpty || (data && data[data.length - 1]?.length < 6);
+  // const isRefreshing = isValidating && data && data.length === size;
   return (
     <>
       <Head>
@@ -56,7 +56,7 @@ const WhaleAlerts = () => {
             <Grid item xs={12} md={12}>
               <WhaleAlertListResults/>
               
-              {isEmpty ? <p>Yay, no transactions found.</p> : null}
+              {/* {isEmpty ? <p>Yay, no transactions found.</p> : null} */}
             </Grid>
             {/* <button onClick={() => setSize(size + 1)}>Load More</button> */}
           </Box>
