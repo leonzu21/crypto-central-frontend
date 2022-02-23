571e1ebbf41ee5dbfc102e300190026126060a92
Iulian Leon <iulian.leon@philro.ro>
2022-02-23 21:09:20 +0200

    add filter by symbol

--- src/components/whale-alert/whale-alert-list-results.js
+++ src/components/whale-alert/whale-alert-list-results.js
@@ -1,47 +1,53 @@
 import { useState, useEffect } from "react";
 import PerfectScrollbar from "react-perfect-scrollbar";
-import PropTypes from "prop-types";
 import useSWRInfinite from "swr/infinite";
 import WhaleAlertRow from "./whale-alert-row";
 import "react-perfect-scrollbar/dist/css/styles.css";
 
-
 import DatePicker from "@mui/lab/DatePicker";
 
-import { format } from "date-fns";
 import {
-  Avatar,
   Box,
   Card,
-  Checkbox,
   Table,
   TableBody,
   TableCell,
   TableHead,
-  TablePagination,
   TableRow,
-  Typography,
   Button,
-  ButtonGroup,
+  ToggleButton,
+  ToggleButtonGroup,
   TextField,
   Grid,
+  CardHeader,
+  Autocomplete,
 } from "@mui/material";
 
 import { GetCurrentDate } from "src/utils/get-current-date";
 
-
 const fetcher = (url) => fetch(url).then((res) => res.json());
 
-export const WhaleAlertListResults = ({ ...rest }) => {
-  const [endpoint, setEndpoint] = useState(
-    `findAllByDayOrderByAmountDesc?theDate=${GetCurrentDate()["currDate"]}`
-  );
+export const WhaleAlertListResults = ({ propSymbol, ...rest }) => {
   const [date, setDate] = useState(null);
   const [day, setDay] = useState(null);
   const [month, setMonth] = useState(null);
   const [year, setYear] = useState(null);
   const [value, setValue] = useState(Date());
   const [filterBy, setFilterBy] = useState("Day");
+  const [symbol, setSymbol] = useState(propSymbol ? propSymbol : "");
+  const [symbolValue, setSymbolValue] = useState(propSymbol ? propSymbol : null);
+  const [bySymbol, setBySymbol] = useState(propSymbol ? "BySymbol" : "");
+  const [coins, setCoins] = useState(null);
+  const [endpoint, setEndpoint] = useState(
+    `findAll${bySymbol}ByDayOrderByAmountDesc?theDate=${
+      GetCurrentDate()["currDate"]
+    }`
+  );
+  const [alignment, setAlignment] = useState("24h");
+
+  const handleAlignment = (event, newAlignment) => {
+    setAlignment(newAlignment);
+  };
 
   useEffect(() => {
     const currentDate = GetCurrentDate();
@@ -52,9 +58,17 @@ export const WhaleAlertListResults = ({ ...rest }) => {
       setMonth(currentDate["month"]);
       setYear(currentDate["year"]);
       setEndpoint(
-        `findAllByDayOrderByAmountDesc?theDate=${currentDate["year"]}-${currentDate["month"]}-${currentDate["day"]}`
+        `findAll${bySymbol}ByDayOrderByAmountDesc?theDate=${currentDate["year"]}-${currentDate["month"]}-${currentDate["day"]}`
       );
     }
+  }, []);
+
+  // 3. Create out useEffect function
+  useEffect(() => {
+    fetch("https://dmc8ptcuv1dn8.cloudfront.net/api/symbols/all")
+      .then((response) => response.json())
+      // 4. Setting *dogImage* to the image url that we received from the response above
+      .then((data) => setCoins(data));
   }, []);
 
   const { data, error, mutate, size, setSize, isValidating } = useSWRInfinite(
@@ -90,40 +104,54 @@ export const WhaleAlertListResults = ({ ...rest }) => {
 
   return (
     <Card {...rest}>
+      <CardHeader title="Biggest Whale Alerts" />
       <Box sx={{ m: 1 }}>
         <Grid container>
-          <Grid item xs={8} md={8}>
-            <ButtonGroup size="small" color="primary" aria-label="small group">
-              <Button
+          <Grid item xs={8} md={4} sx={{ mb: { xs: 1 } }}>
+            <ToggleButtonGroup
+              size="small"
+              value={alignment}
+              exclusive
+              onChange={handleAlignment}
+              aria-label="text alignment"
+            >
+              <ToggleButton
+                sx={{ pl: { md: 2, xs: 1 }, pr: { md: 2, xs: 1 } }}
+                value="24h"
                 onClick={() => {
-                  console.log(`[Daily onClick] -----> ${day}`);
                   setEndpoint(
-                    `findAllByDayOrderByAmountDesc?theDate=${year}-${month}-${day}`
+                    `findAll${bySymbol}ByDayOrderByAmountDesc?theDate=${year}-${month}-${day}${symbol}`
                   );
                   setFilterBy("Day");
                 }}
               >
-                1 Day
-              </Button>
-              <Button
+                24h
+              </ToggleButton>
+              <ToggleButton
+                sx={{ pl: { md: 2, xs: 1 }, pr: { md: 2, xs: 1 } }}
+                value="30d"
                 onClick={() => {
                   setEndpoint(
-                    `findAllByMonthOrderByAmountDesc?theDate=${year}-${month}`
+                    `findAll${bySymbol}ByMonthOrderByAmountDesc?theDate=${year}-${month}${symbol}`
                   );
                   setFilterBy("Month");
                 }}
               >
-                1 Month
-              </Button>
-              <Button
+                30d
+              </ToggleButton>
+              <ToggleButton
+                sx={{ pl: { md: 2, xs: 1 }, pr: { md: 2, xs: 1 } }}
+                value="1y"
                 onClick={() => {
-                  setEndpoint(`findAllByYearOrderByAmountDesc?theDate=${year}`);
+                  setEndpoint(
+                    `findAll${bySymbol}ByYearOrderByAmountDesc?theDate=${year}${symbol}`
+                  );
                   setFilterBy("Year");
                 }}
               >
-                1 Year
-              </Button>
-            </ButtonGroup>
+                1y
+              </ToggleButton>
+            </ToggleButtonGroup>
           </Grid>
           <Grid item xs={4} md={4}>
             <DatePicker
@@ -135,37 +163,88 @@ export const WhaleAlertListResults = ({ ...rest }) => {
                 calMonth = calMonth < 10 ? `0${calMonth}` : `${calMonth}`;
                 let calYear = newValue.getFullYear();
                 setValue(newValue);
+                console.log(symbol);
                 switch (filterBy) {
                   case "Day":
                     setEndpoint(
-                      `findAllBy${filterBy}OrderByAmountDesc?theDate=${calYear}-${calMonth}-${calDay}`
+                      `findAll${bySymbol}By${filterBy}OrderByAmountDesc?theDate=${calYear}-${calMonth}-${calDay}${symbol}`
                     );
                     break;
                   case "Month":
                     setEndpoint(
-                      `findAllBy${filterBy}OrderByAmountDesc?theDate=${calYear}-${calMonth}`
+                      `findAll${bySymbol}By${filterBy}OrderByAmountDesc?theDate=${calYear}-${calMonth}${symbol}`
                     );
                     break;
                   case "Year":
                     setEndpoint(
-                      `findAllBy${filterBy}OrderByAmountDesc?theDate=${calYear}`
+                      `findAll${bySymbol}By${filterBy}OrderByAmountDesc?theDate=${calYear}${symbol}`
                     );
                     break;
                   default:
                     setEndpoint(
-                      `findAllBy${filterBy}OrderByAmountDesc?theDate=${calYear}-${calMonth}-${calYear}`
+                      `findAll${bySymbol}By${filterBy}OrderByAmountDesc?theDate=${calYear}-${calMonth}-${calYear}${symbol}`
                     );
                     break;
                 }
+                console.log(endpoint);
                 calDay = calDay < 10 ? `0${calDay}` : `${calDay}`;
                 setDay(calDay);
                 setMonth(calMonth);
                 setYear(calYear);
               }}
               renderInput={(params) => (
-                <TextField size="small" fontSize="small" {...params} />
+                <TextField
+                  size="small"
+                  sx={{ width: { md: 300 } }}
+                  fontSize="small"
+                  {...params}
+                />
               )}
             />
+          </Grid>
+          <Grid item md={4} xs={12}>
+            {coins ? (
+              <Autocomplete
+                size="small"
+                disablePortal
+                id="combo-box-demo"
+                value={symbolValue}
+                options={coins}
+                sx={{ width: { md: 300 } }}
+                onInputChange={(event, newValue) => {
+                  setSymbolValue(newValue);
+                  let symb = newValue ? `&theSymbol=${newValue}` : "";
+                  let bySymb = newValue ? "BySymbol" : "";
+                  setSymbol(symb);
+                  setBySymbol(bySymb);
+                  switch (filterBy) {
+                    case "Day":
+                      setEndpoint(
+                        `findAll${bySymbol}By${filterBy}OrderByAmountDesc?theDate=${year}-${month}-${day}${symb}`
+                      );
+                      break;
+                    case "Month":
+                      setEndpoint(
+                        `findAll${bySymbol}By${filterBy}OrderByAmountDesc?theDate=${year}-${month}${symb}`
+                      );
+                      break;
+                    case "Year":
+                      setEndpoint(
+                        `findAll${bySymbol}By${filterBy}OrderByAmountDesc?theDate=${year}${symb}`
+                      );
+                      break;
+                    default:
+                      setEndpoint(
+                        `findAll${bySymbol}By${filterBy}OrderByAmountDesc?theDate=${year}-${month}-${day}${symb}`
+                      );
+                      break;
+                  }
+
+                  console.log(endpoint);
+                }}
+                renderInput={(params) => <TextField {...params} label="All" />}
+              />
+            ) : null}
           </Grid>
         </Grid>
       </Box>
