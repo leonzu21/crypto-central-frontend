9cefff49c2504898b72a5aed7f61fc46ee1ef3bb
Iulian Leon <iulian.leon@philro.ro>
2022-03-22 13:07:09 +0200

    edit menu icons

--- src/components/dashboard-sidebar.js
+++ src/components/dashboard-sidebar.js
@@ -5,14 +5,12 @@ import PropTypes from 'prop-types';
 import { Box, Button, Divider, Drawer, Typography, useMediaQuery } from '@mui/material';
 import OpenInNewIcon from '@mui/icons-material/OpenInNew';
 import { ChartBar as ChartBarIcon } from '../icons/chart-bar';
-import { Cog as CogIcon } from '../icons/cog';
-import { Lock as LockIcon } from '../icons/lock';
 import { Selector as SelectorIcon } from '../icons/selector';
-import { ShoppingBag as ShoppingBagIcon } from '../icons/shopping-bag';
 import { User as UserIcon } from '../icons/user';
-import { UserAdd as UserAddIcon } from '../icons/user-add';
 import { Users as UsersIcon } from '../icons/users';
-import { XCircle as XCircleIcon } from '../icons/x-circle';
+import PhishingIcon from '@mui/icons-material/Phishing';
+import CurrencyBitcoinIcon from '@mui/icons-material/CurrencyBitcoin';
+import FiberNewIcon from '@mui/icons-material/FiberNew';
 import { Logo } from './logo';
 import { NavItem } from './nav-item';
 
@@ -24,39 +22,39 @@ const items = [
   },
   {
     href: '/coins',
-    icon: (<UsersIcon fontSize="small" />),
+    icon: (<CurrencyBitcoinIcon fontSize="small" />),
     title: 'Coins'
   },
   {
     href: '/whale-alerts',
-    icon: (<ShoppingBagIcon fontSize="small" />),
+    icon: (<PhishingIcon fontSize="small" />),
     title: 'Whale Alerts'
   },
   {
     href: '/account',
-    icon: (<UserIcon fontSize="small" />),
-    title: 'Account'
+    icon: (<FiberNewIcon fontSize="small" />),
+    title: 'ICO Drops'
   },
-  {
-    href: '/settings',
-    icon: (<CogIcon fontSize="small" />),
-    title: 'Settings'
-  },
-  {
-    href: '/login',
-    icon: (<LockIcon fontSize="small" />),
-    title: 'Login'
-  },
-  {
-    href: '/register',
-    icon: (<UserAddIcon fontSize="small" />),
-    title: 'Register'
-  },
-  {
-    href: '/404',
-    icon: (<XCircleIcon fontSize="small" />),
-    title: 'Error'
-  }
+  // {
+  //   href: '/settings',
+  //   icon: (<CogIcon fontSize="small" />),
+  //   title: 'Settings'
+  // },
+  // {
+  //   href: '/login',
+  //   icon: (<LockIcon fontSize="small" />),
+  //   title: 'Login'
+  // },
+  // {
+  //   href: '/register',
+  //   icon: (<UserAddIcon fontSize="small" />),
+  //   title: 'Register'
+  // },
+  // {
+  //   href: '/404',
+  //   icon: (<XCircleIcon fontSize="small" />),
+  //   title: 'Error'
+  // }
 ];
 
 export const DashboardSidebar = (props) => {
