7acd5edeb8b8a6467d4f908e48ecf0a5304eb92c
Iulian Leon <iulian.leon@philro.ro>
2022-03-24 21:18:29 +0200

    delete ico

--- src/pages/icos/index.js
+++ src/pages/icos/index.js
@@ -4,80 +4,66 @@ import { Box, Container, Grid, Pagination } from "@mui/material";
 import { DashboardLayout } from "../../components/dashboard-layout";
 import { icoService } from "../../services/ico.service";
 import { IcoListToolbar } from "src/components/ico/ico-list-toolbar";
-import { IcoCard } from '../../components/ico/ico-card';
+import { IcoCard } from "../../components/ico/ico-card";
+import { getHalId } from "src/utils/get_hal_id";
 
 const Icos = () => {
   const [icos, setIcos] = useState(null);
 
   useEffect(() => {
-    icoService.getAll().then(x => setIcos(x._embedded.icoes))
+    icoService.getAll().then((x) => setIcos(x._embedded.icoes));
   }, []);
 
-  console.log(icos);
   function deleteIco(id) {
     setIcos(
       icos.map((x) => {
-        if (x.id === id) {
+        if (getHalId(x) === id) {
           x.isDeleting = true;
         }
         return x;
       })
     );
     icoService.delete(id).then(() => {
-      setIcos((icos) => icos.filter((x) => x.id !== id));
+      setIcos((icos) => icos.filter((x) => getHalId(x) !== id));
     });
   }
 
   return (
     <>
-    <Head>
-      <title>
-        Products | Material Kit
-      </title>
-    </Head>
-    <Box
-      component="main"
-      sx={{
-        flexGrow: 1,
-        py: 8
-      }}
-    >
-      <Container maxWidth={false}>
-        <IcoListToolbar />
-        <Box sx={{ pt: 3 }}>
-          <Grid
-            container
-            spacing={3}
+      <Head>
+        <title>Products | Material Kit</title>
+      </Head>
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          py: 8,
+        }}
+      >
+        <Container maxWidth={false}>
+          <IcoListToolbar />
+          <Box sx={{ pt: 3 }}>
+            <Grid container spacing={3}>
+              {icos &&
+                icos.map((ico) => (
+                  <Grid item key={getHalId(ico)} lg={4} md={6} xs={12}>
+                    <IcoCard deleteIco={deleteIco} ico={ico} />
+                  </Grid>
+                ))}
+            </Grid>
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "center",
+              pt: 3,
+            }}
           >
-            {icos && icos.map((ico) => (
-              <Grid
-                item
-                key={ico.id}
-                lg={4}
-                md={6}
-                xs={12}
-              >
-                <IcoCard ico={ico} />
-              </Grid>
-            ))}
-          </Grid>
-        </Box>
-        <Box
-          sx={{
-            display: 'flex',
-            justifyContent: 'center',
-            pt: 3
-          }}
-        >
-          <Pagination
-            color="primary"
-            count={3}
-            size="small"
-          />
-        </Box>
-      </Container>
-    </Box>
-  </>
+            <Pagination color="primary" count={3} size="small" />
+          </Box>
+        </Container>
+      </Box>
+    </>
   );
 };
 
