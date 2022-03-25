7178650760def381649a1e74e46b357b7fedcf41
Iulian Leon <iulian.leon@philro.ro>
2022-03-25 14:48:37 +0200

    link add project button

--- src/components/ico/ico-list-toolbar.js
+++ src/components/ico/ico-list-toolbar.js
@@ -1,41 +1,46 @@
 import {
-    Box,
-    Button,
-    Card,
-    CardContent,
-    TextField,
-    InputAdornment,
-    SvgIcon,
-    Typography
-  } from '@mui/material';
-  import { Search as SearchIcon } from '../../icons/search';
-  
-  export const IcoListToolbar = (props) => (
-    <Box {...props}>
-      <Box
-        sx={{
-          alignItems: 'center',
-          display: 'flex',
-          justifyContent: 'space-between',
-          flexWrap: 'wrap',
-          m: -1
-        }}
-      >
-        <Typography
-          sx={{ m: 1 }}
-          variant="h4"
-        >
-          Upcomig Projects
-        </Typography>
-        <Box sx={{ m: 1 }}>
+  Box,
+  Button,
+  Card,
+  CardContent,
+  TextField,
+  InputAdornment,
+  SvgIcon,
+  Typography,
+} from "@mui/material";
+import NextLink from "next/link";
+
+import { Search as SearchIcon } from "../../icons/search";
+
+export const IcoListToolbar = (props) => (
+  <Box {...props}>
+    <Box
+      sx={{
+        alignItems: "center",
+        display: "flex",
+        justifyContent: "space-between",
+        flexWrap: "wrap",
+        m: -1,
+      }}
+    >
+      <Typography sx={{ m: 1 }} variant="h4">
+        Upcomig Projects
+      </Typography>
+      <Box sx={{ m: 1 }}>
+        <NextLink href={`/icos/add`}>
           <Button
+            sx={{ p: 0 }}
             color="primary"
             variant="contained"
+            sx={{ mr: 1 }}
           >
             Add project
           </Button>
-        </Box>
+        </NextLink>
+        {/* <Button color="primary" variant="contained">
+          Add project
+        </Button> */}
       </Box>
     </Box>
-  );
-  
+  </Box>
+);
