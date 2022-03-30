052f2248495c780db43c5a4d5a90796cdd19a6ae
Iulian Leon <iulian.leon@philro.ro>
2022-03-30 13:27:35 +0300

    add dynamic theme and start working on next auth

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,20 +1,26 @@
 import Head from "next/head";
 import { CacheProvider } from "@emotion/react";
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useMemo, useContext } from "react";
 import LocalizationProvider from "@mui/lab/LocalizationProvider";
 import AdapterDateFns from "@mui/lab/AdapterDateFns";
 import { CssBaseline } from "@mui/material";
 import { ThemeProvider } from "@mui/material/styles";
 import { createEmotionCache } from "../utils/create-emotion-cache";
-import { theme } from "../theme";
+import { lightTheme, darkTheme } from "../theme";
+import { SessionProvider } from "next-auth/react";
 import "../../public/static/css/index.css";
+import CustomThemeProvider, {
+  CustomThemeContext,
+} from "../context/CustomThemeProvider";
 
 const clientSideEmotionCache = createEmotionCache();
 
-
-
 const App = (props) => {
-  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+  const {
+    Component,
+    emotionCache = clientSideEmotionCache,
+    pageProps: { session, ...pageProps },
+  } = props;
 
   const getLayout = Component.getLayout ?? ((page) => page);
 
@@ -26,24 +32,29 @@ const App = (props) => {
       .then((data) => setCoinsProps(data));
   }, []);
 
+  const ThemeContext = useContext(CustomThemeContext);
+
+  console.log(ThemeContext);
+
   return (
-    <CacheProvider value={emotionCache}>
-      <Head>
-        <title>Material Kit Pro</title>
-        <meta name="viewport" content="initial-scale=1, width=device-width" />
-      </Head>
-      <LocalizationProvider dateAdapter={AdapterDateFns}>
-        <ThemeProvider theme={theme}>
-          <CssBaseline />
-          {getLayout(<Component {...pageProps} {...coinsProps} />)}
-        </ThemeProvider>
-      </LocalizationProvider>
-    </CacheProvider>
+    <SessionProvider session={session}>
+      <CacheProvider value={emotionCache}>
+        <Head>
+          <title>Crypto Central</title>
+          <meta name="viewport" content="initial-scale=1, width=device-width" />
+        </Head>
+        <LocalizationProvider dateAdapter={AdapterDateFns}>
+          <CustomThemeProvider initialAppTheme={ThemeContext.appTheme}>
+            <CssBaseline />
+            {getLayout(<Component {...pageProps} {...coinsProps} />)}
+          </CustomThemeProvider>
+        </LocalizationProvider>
+      </CacheProvider>
+    </SessionProvider>
   );
 };
 
 export default App;
-
 
 // App.getInitialProps = async (Component, ctx) => {
 //   let coins = [];
