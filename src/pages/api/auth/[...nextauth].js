d89373ed9f965abf506bd4c413202136ca1a2eef
Iulian Leon <iulian.leon@philro.ro>
2022-04-01 14:08:56 +0300

    try with auth0

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,13 +1,15 @@
 import NextAuth from "next-auth";
-import GithubProvider from "next-auth/providers/github";
+import Auth0Provider from "next-auth/providers/auth0";
 
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
-    GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
+    Auth0Provider({
+      clientId: process.env.AUTH0_CLIENT_ID,
+      clientSecret: process.env.AUTH0_CLIENT_SECRET,
+      issuer: process.env.AUTH0_ISSUER,
     }),
     // ...add more providers here
   ],
+  secret: process.env.AUTH_SECRET
 });
