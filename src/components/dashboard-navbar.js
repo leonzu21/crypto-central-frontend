88d53600367aa9f429478cda131e74fc25bbaaaa
Iulian Leon <iulian.leon@philro.ro>
2022-04-02 17:28:34 +0300

    test cached session

--- src/components/dashboard-navbar.js
+++ src/components/dashboard-navbar.js
@@ -30,6 +30,8 @@ export const DashboardNavbar = (props) => {
   const { onSidebarOpen, ...other } = props;
   const session = props.usersession;
 
+  console.log(session);
+
   const coins = props.coins;
   const sessionButton = session ? (
     <Button color="error" onClick={() => signOut()}>
