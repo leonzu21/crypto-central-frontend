99fdb59b465aaf0dcaa080476a3a03db0e33c7cc
Iulian Leon <iulian.leon@philro.ro>
2022-04-08 13:17:07 +0300

    play around with dev url

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 const apiUrl = process.env.NODE_ENV === 'development' 
+    // ? 'http://localhost:5000/api' // development api
     ? 'https://dmc8ptcuv1dn8.cloudfront.net/api' // development api
     : 'https://dmc8ptcuv1dn8.cloudfront.net/api'; // production api
 
