2d4e39dfba6c4c216a3aca88816e0f213ff2f0fb
Iulian Leon <iulian.leon@philro.ro>
2022-04-08 18:09:37 +0300

    add multilevel menu items and add portfolio

--- src/components/dashboard-sidebar.js
+++ src/components/dashboard-sidebar.js
@@ -1,4 +1,5 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import React from "react";
 import NextLink from "next/link";
 import { useRouter } from "next/router";
 import PropTypes from "prop-types";
@@ -9,6 +10,11 @@ import {
   Drawer,
   Typography,
   useMediaQuery,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Collapse,
+  List,
 } from "@mui/material";
 import OpenInNewIcon from "@mui/icons-material/OpenInNew";
 import { ChartBar as ChartBarIcon } from "../icons/chart-bar";
@@ -20,12 +26,112 @@ import CurrencyBitcoinIcon from "@mui/icons-material/CurrencyBitcoin";
 import FiberNewIcon from "@mui/icons-material/FiberNew";
 import { Logo } from "./logo";
 import { NavItem } from "./nav-item";
+import { NavItemMultilevel } from "./nav-item-multi";
+
+import ExpandLessIcon from "@mui/icons-material/ExpandLess";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import WorkIcon from '@mui/icons-material/Work';
+import { hasChildren } from "../utils/hasChildren";
+
+const MenuItem = ({ item }) => {
+  const Component = hasChildren(item) ? MultiLevel : SingleLevel;
+  return <Component item={item} />;
+};
+
+const SingleLevel = ({ item }) => {
+  return (
+    <NavItem
+      key={item.title}
+      icon={item.icon}
+      href={item.href}
+      title={item.title}
+    />
+  );
+};
+
+const MultiLevel = ({ item }) => {
+  const { items: children } = item;
+  const [openItem, setOpenItem] = useState(false);
+
+  const handleClick = () => {
+    setOpenItem((prev) => !prev);
+  };
+
+  let expand = null;
+
+  if (openItem) expand = <ExpandLessIcon />;
+  else expand = <ExpandMoreIcon />;
+
+  return (
+    <React.Fragment>
+      <ListItem
+        disableGutters
+        sx={{
+          display: "flex",
+          mb: 0.5,
+          py: 0,
+          px: 2,
+        }}
+      >
+        <Button
+          onClick={handleClick}
+          component="a"
+          startIcon={item.icon}
+          disableRipple
+          sx={{
+            backgroundColor: openItem && "rgba(255,255,255, 0.08)",
+            borderRadius: 1,
+            color: openItem ? "primary.main" : "neutral.300",
+            fontWeight: openItem && "fontWeightBold",
+            justifyContent: "flex-start",
+            px: 3,
+            textAlign: "left",
+            textTransform: "none",
+            width: "100%",
+            "& .MuiButton-startIcon": {
+              color: openItem ? "primary.main" : "neutral.400",
+            },
+            "&:hover": {
+              backgroundColor: "rgba(255,255,255, 0.08)",
+            },
+          }}
+        >
+          <Box sx={{ flexGrow: 1 }}>
+            {item.title}
+          </Box>
+          <Box sx={{ flexGrow: 0 }}>
+            {expand}
+          </Box>
+        </Button>
+      </ListItem>
+      <Collapse in={openItem} timeout="auto" unmountOnExit>
+        <List component="div" disablePadding>
+          {children.map((child, key) => (
+            <MenuItem key={key} item={child} />
+          ))}
+        </List>
+      </Collapse>
+    </React.Fragment>
+  );
+};
 
 const items = [
   {
-    href: "/",
+    // href: "/",
     icon: <ChartBarIcon fontSize="small" />,
     title: "Dashboard",
+    items: [
+      {
+        href: "/icos",
+        icon: <FiberNewIcon fontSize="small" />,
+        title: "My ICOs",
+      },
+      {
+        href: "/portfolio",
+        icon: <WorkIcon fontSize="small" />,
+        title: "Portfolio",
+      },
+    ],
   },
   {
     href: "/coins",
@@ -37,11 +143,11 @@ const items = [
     icon: <PhishingIcon fontSize="small" />,
     title: "Whale Alerts",
   },
-  {
-    href: "/icos",
-    icon: <FiberNewIcon fontSize="small" />,
-    title: "ICO Drops",
-  },
+  // {
+  //   href: "/icos",
+  //   icon: <FiberNewIcon fontSize="small" />,
+  //   title: "ICO Drops",
+  // },
   // {
   //   href: '/settings',
   //   icon: (<CogIcon fontSize="small" />),
@@ -138,12 +244,13 @@ export const DashboardSidebar = (props) => {
         />
         <Box sx={{ flexGrow: 1 }}>
           {items.map((item) => (
-            <NavItem
-              key={item.title}
-              icon={item.icon}
-              href={item.href}
-              title={item.title}
-            />
+            // <NavItem
+            //   key={item.title}
+            //   icon={item.icon}
+            //   href={item.href}
+            //   title={item.title}
+            // />
+            <MenuItem key={item.title} item={item} />
           ))}
         </Box>
         <Divider sx={{ borderColor: "#2D3748" }} />
